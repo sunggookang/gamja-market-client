a5af6d92573975df98190338dddc75445b69d564
sunggookang <rkdtjdrn90@gmail.com>
2021-08-23 15:44:52 +0900

    antd 기본 적용

--- src/product/index.js
+++ src/product/index.js
@@ -1,11 +1,39 @@
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './index.css';
 
 function ProductPage() {
     const { id } = useParams();
+    const [product, setProduct] = useState(null);
+    useEffect(function () {
+        axios.get(`https://b17784ea-92b6-4a82-9735-16a94f4cb393.mock.pstmn.io/products/${id}`).then(function (result) {
+            setProduct(result.data);
+        }).catch(function (error) {
+            console.error('에러 발생: ', error);
+        })
+    }, []);
+
+    if (product === null) {
+        return <h1>상품 정보를 받고 있습니다...</h1>
+    }
+
     return (
-        <h1>
-            상품 상세 페이지 {id} 상품
-        </h1>
+        <div>
+            <div id="image-box">
+                <img src={`/${product.imageUrl}`} />
+            </div>
+            <div id="profile-box">
+                <img src="/images/icons/avatar.png" />
+                <span>{product.seller}</span>
+            </div>
+            <div id="contents-box">
+                <div id="name">{product.name}</div>
+                <div id="price">{product.price}원</div>
+                <div id="createdAt">2021년 8월 23일</div>
+                <div id="description">{product.description}</div>
+            </div>
+        </div>
     )
 }
 
